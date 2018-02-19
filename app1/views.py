c75794d92f3b31c53385105520c69f595b683258
Mikhail T <m.tkachev@magdv.com>
2018-02-19 14:21:04 +0700

    * Added SSH executor * Changed reset session mechanism

--- app1/views.py
+++ app1/views.py
@@ -7,6 +7,7 @@ import datetime
 
 from app1.sql_executor import SqlExec
 from app1.test_data import private_data, individual_data, business_data
+from app1.ssh_executor import SshExecutor
 
 
 def ping(request):    
@@ -21,26 +22,20 @@ def set_stand(request, stand):
     return ping(request) 
     
     
-def reset_session(request, session_id, timestamp=None):           
-    if timestamp is None:
-        timestamp = time.mktime((datetime.date.today() - datetime.timedelta(days=30)).timetuple())        
-    
+def reset_session(request, session_id):           
     stand = request.session.get('stand', 'mobile')
-    sql_command = "update core_session set session_expires = {} where session_id = '{}'".format(int(timestamp), session_id)
-    executor = SqlExec(stand)
+        
+    executor = SshExecutor(stand)
     error = None
-    try: 
-        executor.execute(sql_command)
-        executor.commit()
+    try:
+        executor.execute("cd /opt/stand/magonline/%s \ndc exec sesredis redis-cli -n 2 eval \"return redis.call('del', 'sess_%s')\" 0 prefix" % (stand, session_id))         
     except Exception, msg: 
         error = msg 
     executor.close()
     
     response_data = {"message": "OK" if error is None else "'{}'".format(error),
                      "stand" : stand,
-                     "data": {"session_id": session_id,
-                              "session_expired": timestamp,
-                              "session_expired_str": datetime.datetime.fromtimestamp(float(timestamp)).strftime('%Y-%m-%d %H:%M:%S')}
+                     "data": {"session_id": session_id}
                      }    
     return HttpResponse(json.dumps(response_data), content_type="application/json")
    
