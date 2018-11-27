9bf37729a47516b348cc51899b37b567078d3801
Mikhail T <m.tkachev@magdv.com>
2018-11-27 14:30:25 +0700

    * Updated Private user creation SQL

--- app1/test_data.py
+++ app1/test_data.py
@@ -15,37 +15,76 @@ def private_data():
             "password":"qwer1234",
             "phone":"7913{}".format(phone),
             "user_type": "private",
-            "address": "Томск, пр. Комсомольский 90, кв. 34, {}".format(payloads),
+            "address": "г Томск, пр-кт Ленина, д 232, кв 32, {}".format(payloads),
             }
     
     sql_command = """
-insert into customer_entity (entity_type_id, attribute_set_id, website_id, email, group_id, increment_id, store_id, created_at, updated_at, is_active, disable_auto_group_change, warehouse_id) values (1, 0, 2, '{0}', 15, null, 2, str_to_date('2017-10-29 04:30:17', '%Y-%m-%d %T'), str_to_date('2017-10-29 11:38:24', '%Y-%m-%d %T'), 1, 0, 1);
+INSERT INTO customer_entity (entity_type_id, attribute_set_id, website_id, email, group_id, increment_id, store_id, created_at, updated_at, is_active, disable_auto_group_change, warehouse_id, is_phone_valid, phone, locale_code, is_test_account)
+VALUES (1, 0, 2, '{0}', 15, null, 2, '2017-12-06 00:15:00', '2018-04-19 10:53:56', 1, 0, 1, 0, '{4}', 'ru_RU', 0);
 SET @user_id = LAST_INSERT_ID();
-insert into customer_address_entity (entity_type_id, attribute_set_id, increment_id, parent_id, created_at, updated_at, is_active, warehouse_id) values (2, 0, null, @user_id, str_to_date('2017-10-23 02:29:17', '%Y-%m-%d %T'), str_to_date('2017-10-23 02:29:17', '%Y-%m-%d %T'), 1, 2);
+
+INSERT INTO customer_address_entity (entity_type_id, attribute_set_id, increment_id, parent_id, created_at, updated_at, is_active, warehouse_id, id_point_sales) 
+VALUES (2, 0, null, @user_id, '2017-12-04 02:01:26', '2018-04-19 10:53:56', 1, 1, null);
+
 SET @address_id = LAST_INSERT_ID();
 SET @user_email = (SELECT email FROM customer_entity WHERE entity_id = @user_id);
-insert into customer_entity_varchar (entity_type_id, attribute_id, entity_id, value) values (1, 3, @user_id, '{1}');
-insert into customer_entity_varchar (entity_type_id, attribute_id, entity_id, value) values (1, 5, @user_id, '{2}');
-insert into customer_entity_varchar (entity_type_id, attribute_id, entity_id, value) values (1, 7, @user_id, '{3}');
-insert into customer_entity_varchar (entity_type_id, attribute_id, entity_id, value) values (1, 12, @user_id, '19bb65710a8feb4176641456f217431a:lYCWGgalauLBZJPRB9mzf1fkU3EoJp7R');
-insert into customer_entity_varchar (entity_type_id, attribute_id, entity_id, value) values (1, 210, @user_id, '1');
-insert into customer_entity_varchar (entity_type_id, attribute_id, entity_id, value) values (1, 269, @user_id, '{4}');
-insert into customer_entity_int (entity_type_id, attribute_id, entity_id, value) values (1, 14, @user_id, @address_id);
-insert into customer_entity_int (entity_type_id, attribute_id, entity_id, value) values (1, 286, @user_id, 1);
-insert into customer_entity_int (entity_type_id, attribute_id, entity_id, value) values (1, 287, @user_id, 1);
-insert into customer_entity_int (entity_type_id, attribute_id, entity_id, value) values (1, 295, @user_id, 1);
-insert into newsletter_subscriber (store_id, change_status_at, customer_id, subscriber_email, subscriber_status, subscriber_confirm_code) values (2, null, @user_id, @user_email, 1, 'wxd7qbgjl3hmk8zjjqybjwyck9aebs49');
-insert into customer_address_entity_int (entity_type_id, attribute_id, entity_id, value) values (2, 29, @address_id, 0);
-insert into customer_address_entity_int (entity_type_id, attribute_id, entity_id, value) values (2, 266, @address_id, 0);
-insert into customer_address_entity_int (entity_type_id, attribute_id, entity_id, value) values (2, 292, @address_id, 1);
-insert into customer_address_entity_varchar (entity_type_id, attribute_id, entity_id, value) values (2, 22, @address_id, '{2}');
-insert into customer_address_entity_varchar (entity_type_id, attribute_id, entity_id, value) values (2, 20, @address_id, '{3}');
-insert into customer_address_entity_varchar (entity_type_id, attribute_id, entity_id, value) values (2, 27, @address_id, 'RU');
-insert into customer_address_entity_varchar (entity_type_id, attribute_id, entity_id, value) values (2, 31, @address_id, '{4}');
-insert into customer_address_entity_varchar (entity_type_id, attribute_id, entity_id, value) values (2, 293, @address_id, '56.4834717');
-insert into customer_address_entity_varchar (entity_type_id, attribute_id, entity_id, value) values (2, 294, @address_id, '84.9813853');
-insert into customer_address_entity_text (entity_type_id, attribute_id, entity_id, value) values (2, 25, @address_id, "{5}");
-insert into customer_address_entity_text (entity_type_id, attribute_id, entity_id, value) values (2, 264, @address_id, "");
+
+INSERT INTO customer_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (1, 3, @user_id, '{1}');
+INSERT INTO customer_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (1, 4, @user_id, null);
+INSERT INTO customer_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (1, 5, @user_id, '{2}');
+INSERT INTO customer_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (1, 6, @user_id, null);
+INSERT INTO customer_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (1, 7, @user_id, '{3}');
+INSERT INTO customer_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (1, 8, @user_id, null);
+INSERT INTO customer_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (1, 12, @user_id, '19bb65710a8feb4176641456f217431a:lYCWGgalauLBZJPRB9mzf1fkU3EoJp7R');
+INSERT INTO customer_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (1, 15, @user_id, null);
+INSERT INTO customer_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (1, 210, @user_id, '1');
+INSERT INTO customer_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (1, 224, @user_id, null);
+INSERT INTO customer_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (1, 268, @user_id, null);
+INSERT INTO customer_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (1, 269, @user_id, '{4}');
+INSERT INTO customer_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (1, 270, @user_id, null);
+INSERT INTO customer_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (1, 272, @user_id, null);
+INSERT INTO customer_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (1, 273, @user_id, null);
+INSERT INTO customer_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (1, 274, @user_id, null);
+INSERT INTO customer_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (1, 275, @user_id, null);
+INSERT INTO customer_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (1, 276, @user_id, null);
+INSERT INTO customer_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (1, 277, @user_id, null);
+INSERT INTO customer_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (1, 278, @user_id, '0');
+INSERT INTO customer_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (1, 279, @user_id, null);
+INSERT INTO customer_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (1, 280, @user_id, null);
+INSERT INTO customer_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (1, 283, @user_id, null);
+
+INSERT INTO customer_entity_int (entity_type_id, attribute_id, entity_id, value) VALUES (1, 14, @user_id, @address_id);
+INSERT INTO customer_entity_int (entity_type_id, attribute_id, entity_id, value) VALUES (1, 286, @user_id, 1);
+INSERT INTO customer_entity_int (entity_type_id, attribute_id, entity_id, value) VALUES (1, 287, @user_id, 1);
+INSERT INTO customer_entity_int (entity_type_id, attribute_id, entity_id, value) VALUES (1, 295, @user_id, 1);
+
+INSERT INTO customer_address_entity_int (entity_type_id, attribute_id, entity_id, value) VALUES (2, 29, @address_id, 0);
+INSERT INTO customer_address_entity_int (entity_type_id, attribute_id, entity_id, value) VALUES (2, 266, @address_id, 0);
+INSERT INTO customer_address_entity_int (entity_type_id, attribute_id, entity_id, value) VALUES (2, 292, @address_id, 1);
+
+INSERT INTO customer_address_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (2, 19, @address_id, null);
+INSERT INTO customer_address_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (2, 20, @address_id, '{2}');
+INSERT INTO customer_address_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (2, 21, @address_id, null);
+INSERT INTO customer_address_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (2, 22, @address_id, '{3}');
+INSERT INTO customer_address_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (2, 23, @address_id, null);
+INSERT INTO customer_address_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (2, 24, @address_id, null);
+INSERT INTO customer_address_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (2, 26, @address_id, null);
+INSERT INTO customer_address_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (2, 27, @address_id, 'RU');
+INSERT INTO customer_address_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (2, 28, @address_id, null);
+INSERT INTO customer_address_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (2, 30, @address_id, null);
+INSERT INTO customer_address_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (2, 31, @address_id, '{4}');
+INSERT INTO customer_address_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (2, 32, @address_id, null);
+INSERT INTO customer_address_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (2, 36, @address_id, null);
+INSERT INTO customer_address_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (2, 263, @address_id, null);
+INSERT INTO customer_address_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (2, 265, @address_id, null);
+INSERT INTO customer_address_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (2, 267, @address_id, null);
+INSERT INTO customer_address_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (2, 293, @address_id, '56.4907382');
+INSERT INTO customer_address_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (2, 294, @address_id, '84.948149');
+INSERT INTO customer_address_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (2, 321, @address_id, '3');
+INSERT INTO customer_address_entity_varchar (entity_type_id, attribute_id, entity_id, value) VALUES (2, 322, @address_id, '7');
+
+INSERT INTO customer_address_entity_text (entity_type_id, attribute_id, entity_id, value) VALUES (2, 25, @address_id, '{5}');
+INSERT INTO customer_address_entity_text (entity_type_id, attribute_id, entity_id, value) VALUES (2, 264, @address_id, '');
 """.format(user["email"], user["sity"], user["lastname"], user["name"], user["phone"], user["address"])
 
     return (sql_command, user) 
